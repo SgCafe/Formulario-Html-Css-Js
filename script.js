745a24bc8489275c0e75f0815dff3746eb949f93
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2022-10-07 11:48:08 -0300

    feat: finish project

---
+++ script.js
@@ -0,0 +1,73 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordConfirmation = document.getElementById('password-confirmation')
+
+//Fazer ação de não carregar a página quando enviar o formulario
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    checkInputs()
+})
+
+function checkInputs(){
+    const usernameValue = username.value
+    const emailValue = email.value
+    const passwordValue = password.value
+    const passwordConfirmationValue = passwordConfirmation.value
+
+    if(usernameValue === ""){
+        setErrorFor(username, "O nome de usuário é obrigatório.")
+    }else{
+        setSuccessFor(username)
+    }
+    
+    if(emailValue === ""){
+        setErrorFor(email, "O email é obrigatório.")
+    }else if (!checkEmail(emailValue)){
+        setErrorFor(email, "Por favor, insira um email válido.")
+
+    }else{
+        setSuccessFor(email)
+    }
+
+    if(passwordValue === ""){
+        setErrorFor(password, "A senha é obrigatória.")
+    }else if(passwordValue.length < 8){
+        setErrorFor(password, "A senha precisa ter no minimo 8 caracteres.")
+    }else{
+        setSuccessFor(password)
+    }
+
+    if(passwordConfirmationValue === ""){
+        setErrorFor(passwordConfirmation, "O nome de usuário é obrigatório.")
+    }else if(passwordConfirmationValue != passwordValue){
+        setErrorFor(passwordConfirmation, "As senhas precisam ser iguais.")
+    }else{
+        setSuccessFor(passwordConfirmation)
+    }
+}
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+
+    formControl.className = "form-control success"
+
+}
+
+
+// Esse cara retorna true se o email for válido e false se for invalido
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
